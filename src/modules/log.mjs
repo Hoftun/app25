2834fbab16d077729f67406f77176554e3d8c436
hoftun <ingridhoftun97@gmail.com>
2025-02-09 22:52:26 +0100

    Added 3 different log levels (Now the server works)

--- src/modules/log.mjs
+++ src/modules/log.mjs
@@ -1,15 +1,24 @@
-const log = function (isEnabled)  {
+//Enums for log levels
+let level_id = 0;
+const LOGG_LEVELS = {
+  VERBOSE: ++level_id,
+  IMPORTANT: ++level_id,
+  ALWAYS: ++level_id,
+};
+
+let currentGlobalLogLevel = LOGG_LEVELS.VERBOSE;
+
+const log = function (loggLevel)  {
 
   return function (req, res, next) {
-    if (isEnabled) {
+    if (loggLevel >= currentGlobalLogLevel) {
       console.log(`${Date.now()}|${req.method}|${req.url}`);
     }
-  
+    next();
   }
-  // next();
+  
 };
 
 
 export default log;
 
-//en lett refactor som gir oss mulighet til å skru av all logging på en gang
