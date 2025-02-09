5bf1be6fce7acc4330b467b782772a94cfc9ed4e
hoftun <ingridhoftun97@gmail.com>
2025-02-09 22:24:03 +0100

    Uses pipes in logger instead

--- src/modules/log.mjs
+++ src/modules/log.mjs
@@ -1,5 +1,5 @@
 const log = function (req, res, next) {
-    console.log(`${Date.now()} ${req.method} ${req.url}`);
+    console.log(`${Date.now()}|${req.method}|${req.url}`);
     next();
     
 };
