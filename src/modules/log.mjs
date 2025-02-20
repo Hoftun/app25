b6324132e33cfb3bc3e605499183f9e132b9f1f5
hoftun <ingridhoftun97@gmail.com>
2025-02-20 16:49:11 +0100

    Added logging functions (logVerbose, logImportant, logAlways) to log.mjs

--- src/modules/log.mjs
+++ src/modules/log.mjs
@@ -10,14 +10,37 @@ let currentGlobalLogLevel = LOGG_LEVELS.VERBOSE;
 
 const log = function (loggLevel)  {
 
-  return function (req, res, next) {
-    if (loggLevel >= currentGlobalLogLevel) {
-      console.log(`${Date.now()}|${req.method}|${req.url}`);
-    }
-    next();
-  }
+
+
+
+  return 
   
-};
+}
+
+const logVerbose = (req, res, next) =>{
+  if (LOGG_LEVELS.VERBOSE == currentGlobalLogLevel) {
+    printLog(req, res);
+  }
+  next();
+}
+const logImportant = (req, res, next) =>{
+  if (LOGG_LEVELS.IMPORTANT == currentGlobalLogLevel) {
+      printLog(req, res);
+  }
+  next();
+}
+
+const logAlways = (req, res, next) =>{
+  if (LOGG_LEVELS.ALWAYS == currentGlobalLogLevel) {
+    printLog(req, res);
+  }
+  next();
+}
+
+const printLog = (req, res) => {
+  console.log(`${Date.now()}|${req.method}|${req.url}`);
+}
+
 
 
 export default log;
