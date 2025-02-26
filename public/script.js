2217c299c325862fbeab9949ca69a2354c91c5d7
hoftun <ingridhoftun97@gmail.com>
2025-02-26 23:56:45 +0100

    Added a service-worker for cashing and online support

--- public/script.js
+++ public/script.js
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => console.log('Service Worker registered!', reg))
+        .catch(err => console.error('Service Worker registration failed!', err));
+    });
+  }
+  
 let minutes = 25;
 let seconds = 0;
 let timerInterval;
---
+++ public/service-worker.js
@@ -0,0 +1,44 @@
+const cacheName = "pomodoro-cache-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/script.js",
+  "/styles.css",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Install event: Cache all the assets
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: Clear old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== cacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event: Serve cached assets
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
