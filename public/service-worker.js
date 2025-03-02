6ee3f27ced89162871af98ee655b1b973ebbe05e
hoftun <ingridhoftun97@gmail.com>
2025-03-02 20:28:27 +0100

    Updated Service Worker to prevent caching issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,48 +1,41 @@
+const cacheName = "pomodoro-cache-v2";  
 
-console.log("Service worker is installing...");
-
-const cacheName = "pomodoro-cache-v1";
 const assets = [
   "/",
   "/index.html",
+  "/style.css",  
   "/script.js",
-  "/style.css",
   "/manifest.json",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-  "/images/cat.GIF"
+  "/icons/icon-512x512.png"
 ];
 
-// Install event: Cache all the assets
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(assets);
     })
   );
-  self.skipWaiting();
+  self.skipWaiting(); 
 });
 
-// Activate event: Clear old caches
+// Activate event: Delete old caches
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys
-          .filter(key => key !== cacheName)
-          .map(key => caches.delete(key))
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
       );
     })
   );
-  self.clients.claim();
+  self.clients.claim(); 
 });
 
-// Fetch event: Serve cached assets
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
+    fetch(event.request) 
+      .catch(() => caches.match(event.request))
   );
 });
-
