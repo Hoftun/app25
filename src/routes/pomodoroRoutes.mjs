a41c079866c89f669fef4abfaa6b176cb6ef0d19
hoftun <ingridhoftun97@gmail.com>
2025-03-12 21:12:01 +0100

    Updated pomodoroRoutes to support Today's History and Lifetime Stats

--- src/routes/pomodoroRoutes.mjs
+++ src/routes/pomodoroRoutes.mjs
@@ -3,6 +3,8 @@ import {
     getSessions,
     getSession,
     createNewSession,
+    getTodaySessions,
+    getLifetimeStats,
     updateExistingSession,
     deleteExistingSession
 } from "../controllers/pomodoroController.mjs";
@@ -10,9 +12,18 @@ import {
 const router = express.Router();
 
 router.get("/", getSessions);
+
 router.get("/:id", getSession);
+
 router.post("/", createNewSession);
+
+router.get("/today/:user_id", getTodaySessions);
+
+router.get("/stats/:user_id", getLifetimeStats);
+
 router.put("/:id", updateExistingSession);
+
 router.delete("/:id", deleteExistingSession);
 
 export default router;
+
